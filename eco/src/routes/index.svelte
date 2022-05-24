bc54cad49714a79821bf9c1681c1543435813085
barb1001 <barbarit111@gmail.com>
2022-05-25 00:24:57 +0300

    neww

--- eco/src/routes/index.svelte
+++ eco/src/routes/index.svelte
@@ -1,4 +1,7 @@
 <script>
+import { goto } from "$app/navigation";
+
+
 	const carouselPhotos = [
 		'https://res.cloudinary.com/barb1/image/upload/v1641771584/%D7%91%D7%A8%D7%91%D7%99_4_zvtv3p.gif',
 		'https://res.cloudinary.com/barb1/image/upload/v1641771587/%D7%91%D7%A8%D7%91%D7%99_3_srzbj7.gif',
@@ -27,9 +30,9 @@
 
 <div class="r">
 
-<img  src={carouselPhotos[firstIndex]} alt="barbi" title="מזל טוב לי" />
+<img  src={carouselPhotos[firstIndex]} alt="barbi" title="מזל טוב" />
 </div>
-
+<button on:click={()=> goto("./rikma")}  class="bg-red-400 bt rounded-full p-2 text-yellow-400 hover:scale-125 hover:bg-yellow-400 hover:text-red-400">להזמנת רקמה</button>
 <style>
     .r{
         height: 100vh;
@@ -39,5 +42,10 @@
         align-items: center;
         background-color: #ff83a8;
     }
-    
+    .bt{
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%,-50%);
+      top: 90%;
+    }
 </style>
