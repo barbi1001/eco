72a1f29031682326aeec4ff7567fb67b828f8df7
baruch babayof <58833138+Avi-ADAM@users.noreply.github.com>
2024-12-03 02:32:55 +0200

    fgu

--- eco/svelte.config.js
+++ eco/svelte.config.js
@@ -1,28 +1,17 @@
-const mode = process.argv[2] || 'vercel';
-//import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import vercel from '@sveltejs/adapter-vercel';
 import { preprocessThrelte } from '@threlte/preprocess'
 import seqPreprocessor from 'svelte-sequential-preprocessor'
-import adapterStatic from '@sveltejs/adapter-static';
-import { sveltePreprocess } from 'svelte-preprocess'
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter:  adapterStatic({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false
-		}) ,
-		paths: {
-			base: '',
-		}
+		adapter: vercel(),
 	},
-	prerender: {entries: ['/1/7','/1']},
-	preprocess: seqPreprocessor([sveltePreprocess(), preprocessThrelte()])
+
+	preprocess: seqPreprocessor([preprocess(), preprocessThrelte()])
 };
 
 export default config;
